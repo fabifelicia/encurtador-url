14f886134f84044a2a73148a2e865bbc78792135
fabifelicia <fabianabarretomenezes@gmail.com>
2022-05-27 10:30:21 -0300

    fix eslint

--- src/controller/URLController.ts
+++ src/controller/URLController.ts
@@ -1,36 +1,32 @@
-import { URLModel } from '../database/model/URL';
-import { Request, Response } from 'express'
-import shortId from "shortid";
-import { config } from '../config/Constants';
-
-
+import { URLModel } from "../database/model/URL"
+import { Request, Response } from "express"
+import shortId from "shortid"
+import { config } from "../config/Constants"
 
 export class URLController {
-
-  public async shorten(req: Request, res: Response) : Promise<void> {
-    const { originalUrl } = req.body
-    const url =  await URLModel.findOne({ originalUrl })
+  public async shorten(req: Request, res: Response): Promise<void> {
+    const { originalUrl } = req.body;
+    const url = await URLModel.findOne({ originalUrl })
     if (url) {
       res.json(url)
-      return
+      return;
     }
-    const hash = shortId.generate()
-    const shortUrl = `${config.API_URL}/${hash}`     
+    const hash = shortId.generate();
+    const shortUrl = `${config.API_URL}/${hash}`
     const newUrl = await URLModel.create({ originalUrl, hash, shortUrl })
-    
-    res.json(newUrl)
+
+    res.json(newUrl);
   }
 
-  public async redirect(req: Request, res: Response) : Promise<void> {
-
-    const { hash } = req.params
+  public async redirect(req: Request, res: Response): Promise<void> {
+    const { hash } = req.params;
     const url = await URLModel.findOne({ hash })
 
     if (url) {
       res.redirect(url.originalUrl)
-      return
+      return;
     }
 
-    res.status(400).json({ error: 'URL not found' })
+    res.status(400).json({ error: "URL not found" })
   }
 }
